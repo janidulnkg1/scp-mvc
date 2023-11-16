caaba9ae454feebfd1ebce5486749dfa24b15e60
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-16 17:33:35 +0530

    add changes

--- site-control-panel/site-control-panel/Controllers/HomeController.cs
+++ site-control-panel/site-control-panel/Controllers/HomeController.cs
@@ -28,6 +28,7 @@ namespace site_control_panel.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
