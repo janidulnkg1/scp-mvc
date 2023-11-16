9b57aa97b411a7132416e29d879d1ca445865ce0
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-16 14:51:22 +0530

    Add changes

---
+++ site-control-panel/site-control-panel/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿const password = document.getElementById('password');
+const toggle = document.getElementById('togglePassword');
+
+toggle.addEventListener('change', function () {
+    if (this.checked) {
+        password.type = 'text';
+    } else {
+        password.type = 'password';
+    }
+});
