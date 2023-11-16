97a7b3e7b4d7afde35f15fb16c668a7c76ddda0c
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-16 21:50:38 +0530

    Add Dashboard UI changes

--- site-control-panel/site-control-panel/Controllers/HomeController.cs
+++ site-control-panel/site-control-panel/Controllers/HomeController.cs
@@ -17,12 +17,6 @@ namespace site_control_panel.Controllers
         {
             return View();
         }
-
-        public IActionResult Dashboard()
-        {
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
