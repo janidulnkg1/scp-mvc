5d83ea9be7368193e15205172fd12127258a8b35
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-17 17:38:17 +0530

    Added Changes
    Login AuthEndpoint added

--- site-control-panel/site-control-panel/wwwroot/js/site.js
+++ site-control-panel/site-control-panel/wwwroot/js/site.js
@@ -10,6 +10,53 @@ toggle.addEventListener('change', function () {
     }
 });
 
+// Handle login
+function handleUserLogin(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userData = {
+        username: formData.get('login-username'),
+        password: formData.get('login-password'),
+    };
+
+    const baseAddress = "https://events.quantraxcloud.com/SysControlWebAPI";
+
+    const AuthEndPoint = baseAddress + "/api/Auth/Login";
+
+    fetch(AuthEndPoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": "Basic UVRYQVBJU0VDOkhaOTRzVmtxRCQ3ZW5HXj12UkhLRnVzSl5tbmF5NlNjM1MyZGtQNFZLTXhDJWtRNDJNKlBNckZUQHZRRTclc0VreUdAJXZKVEtFRDdjYU5XK3hOTXhZQnFkR0R4V2txXjJMUzUtREImeDJnR0QjJGdoYkVGVkRxP3M1YWpZK1NwQTJwM1o5VCZHaG1HRWNleWsmRTVuQ0JDTj1AUV5Udk0jWmMyPXBtanFoenVoOHNqNFQzNjNqS3VYPVEhWFZlUlVoKlZrYmV5cXBidVUmUGdfWWJZQDVZdXBZI1UkS1FIdUomSkRqWjMyIyF0Y2NyJCslWkt0RXVVZjRTbUR6Q24=",
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Login failed.');
+            }
+        })
+        .then(data => {
+            
+            alert("Login successful!");
+            setTimeout(() => {
+                window.location.href = '/Dashboard/IsConfiguration';
+            }, 500);
+        })
+        .catch(error => {
+            // Handle failed login
+            alert("Login failed. Please check your credentials.");
+        });
+}
+
+
+
+    $("#loginForm").submit(handleUserLogin);
+});
+
+
 //Table Search Function
 $(document).ready(function () {
     $('.datatable').DataTable({
